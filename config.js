417b349102f5751b2ffd710ab8ba2cf578b42d11
inwardlocus <inwardlocus@gmail.com>
2020-07-15 16:10:07 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+module.exports = {
+    consumer_key: 'YHriEzxmlfie3nykxZjuwwycc',
+    consumer_secret: 'MjAnDwLGJ4NjWS3dgAiYYf6RYHYDy8qxk2bhpfvKHFRuMYMbLr',
+    access_token: '1203488945368993792-iWRlwtuNH1PFwHmGp45sOK2ddS7p2D',
+    access_token_secret: 'o5Pu1gxGkZsLbBzfU7Xr0QbZNFmsDTVtKEWU1gFZAP1Jo'
+  }
+
+ 
